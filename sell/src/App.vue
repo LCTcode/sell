eb51d83ddb3a3d05556abdb4de1a588de28f22d0
LCTcode <605935980@qq.com>
2018-10-13 18:36:25 +0800

    完成vue-resource数据交互与界面大概布局

---
+++ sell/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="nav">
+      <div class="navItem"><router-link to="goods">商品</router-link></div>
+      <div class="navItem"><router-link to="ratings">评论</router-link></div>
+      <div class="navItem"><router-link to="seller">商家</router-link></div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+
+const errOk = 0
+
+export default {
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  created () {
+    this.$http.get('/api/seller').then((res) => {
+      res = res.body
+      if (res.errno === errOk) {
+        this.seller = res.data
+        console.log(this.seller)
+      }
+    })
+  },
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+  .nav{
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    border-bottom: 1px solid rgba(7,17,27,.1);
+  }
+  .navItem{
+    flex: 1;
+    text-align: center;
+  }
+  .navItem a{
+    display: block;
+    font-size: 14px;
+    color: rgb(77,85,93);
+  }
+  .navItem .active{
+    color: red;
+  }
+</style>
---
+++ sell/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router/index'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
