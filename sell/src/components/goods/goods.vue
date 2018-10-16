14bf86be87dbb749e9a42e164e59315371fb1c65
LCTcode <605935980@qq.com>
2018-10-16 21:48:41 +0800

    使用better-scroll完成商品的滚动，以及滚动样式显示

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="goods">
-      <div class="menu-wrapper">
+      <div class="menu-wrapper" ref="menuWrapper">
         <ul>
-          <li v-for="(item,index) in goods" :key="index" class="menu-item">
+          <li v-for="(item,index) in goods" :key="index" class="menu-item" :class="{'current': currentIndex === index}">
             <span class="text">
               <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}</span>
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper">
+      <div class="foods-wrapper" ref="foodsWrapper">
         <ul>
-          <li v-for="(item,index) in goods" class="food-list" :key="index">
+          <li v-for="(item,index) in goods" class="food-list food-list-hook" :key="index">
             <h1 class="title">{{item.name}}</h1>
               <ul>
                 <li v-for="(food,index) in item.foods" class="food-item" :key="index">
@@ -26,12 +26,12 @@
                       {{food.description}}
                     </p>
                     <div class="estra">
-                      <span class="count">月售{{food.sellCount}}份</span>
-                      <span>好评率{{food.rating}}%</span>
+                      <span class="count">月售{{food.sellCount}}份</span><!--
+                     --><span>好评率{{food.rating}}%</span>
                     </div>
                     <div class="price">
-                      <span class="now">￥{{food.price}}</span>
-                      <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                      <span class="now">￥{{food.price}}</span><!--
+                      --><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                     </div>
                   </div>
                 </li>
@@ -43,7 +43,10 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
 const errOk = 0
+
 export default {
   props: {
     seller: {
@@ -52,7 +55,21 @@ export default {
   },
   data () {
     return {
-      goods: {}
+      goods: [],
+      listHeight: [], // 存储滚动的高度
+      scrollY: 0
+    }
+  },
+  computed: {
+    currentIndex () {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i]
+        let height2 = this.listHeight[i + 1]
+        if (!height2 || (this.scrollY > height1 && this.scrollY < height2)) {
+          return i
+        }
+      }
+      return 0
     }
   },
   created () {
@@ -60,9 +77,39 @@ export default {
       res = res.body
       if (res.errno === errOk) {
         this.goods = res.data
+        this.$nextTick(() => {
+          this._initScroll()
+          this._catchHeight()
+        })
+        // this.$nextTick(() => { // 因为vue的异步操作原因，当我们在create中操作dom时，注意在$nextTick
+        //   this._initScroll()
+        //   this._catchHeight()
+        // })
       }
     })
     this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+  },
+  methods: {
+    _initScroll () {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {}) // dom对象&json
+
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType: 3 // better_scroll 当 probeType为 3 的时候，不仅在屏幕滑动的过程中，而且在 momentum 滚动动画运行过程中实时派发 scroll 事件
+      })
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y))
+      })
+    },
+    _catchHeight () { // 获取滚动高度
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook') // 获取节点
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < foodList.length; i++) { // 遍历节点内容
+        let item = foodList[i]
+        height += item.clientHeight // 节点是一个数组，将其遍历内容后的高度相加，以此获得节点高度
+        this.listHeight.push(height)
+      }
+    }
   }
 }
 </script>
@@ -85,6 +132,15 @@ export default {
         height:54px
         width:56px
         padding 0 12px // 80-56)/2 居中
+        &.current // 滚动样式
+          position relative
+          margin-top 1px
+          z-index:10
+          background #ffffff
+          font-weight 700
+          color red
+          .text
+            color: red
         .icon
           display inline-block
           width:12px
@@ -139,16 +195,17 @@ export default {
             line-height 14px
             font-size 14px
             color: rgb(7,17,27)
-          .desc,.estra
-            line-height 12px
+          .desc, .estra
+            line-height 10px
             font-size 10px
             height:12px
             color: rgb(7,17,27)
           .desc
+            line-height 10px
             margin-bottom 8px
           .estra
-            line-height 10px
-            &.count
+            line-height 12px
+            .count
              margin-right 12px
           .price
             font-weight 700
