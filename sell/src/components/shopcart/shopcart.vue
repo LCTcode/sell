f50bf5af8ad889b092fee04828c158f6a7e91f57
LCTcode <605935980@qq.com>
2018-10-17 17:38:58 +0800

    购物车随数据变化而改变样式效果

--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -3,15 +3,20 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-warp">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="num" v-show="totalCount>0">
+            {{totalCount}}
+          </div>
+          <div class="logo" :class="{'lightHeight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'lightHeight':totalCount>0}"></i>
           </div>
         </div>
-        <div class="price">￥ 0</div>
+        <div class="price"  :class="{'lightHeight':totalPrice>0}">￥ {{totalPrice}}</div>
         <div class="desc">配送费 {{deliveryPrice}} ￥</div>
       </div>
       <div class="content-right">
-       ￥ {{minPrice}} 起送
+        <div class="pay" :class="payClass">
+          {{payDesc}}
+        </div>
       </div>
     </div>
   </div>
@@ -23,7 +28,12 @@ export default {
     selectFoods: {
       type: Array,
       default () {
-        return []
+        return [
+          {
+            price: 10,
+            count: 1
+          }
+        ]
       }
     },
     deliveryPrice: {
@@ -33,6 +43,39 @@ export default {
     minPrice: {
       type: Number,
       default: 0
+    }
+  },
+  computed: {
+    totalPrice () {
+      let total = 0
+      this.selectFoods.forEach((food) => {
+        total = food.price * food.count
+      })
+      return total
+    },
+    totalCount () {
+      let count = 0
+      this.selectFoods.forEach((food) => {
+        count += food.count
+      })
+      return count
+    },
+    payDesc () {
+      if (this.totalPrice === 0) {
+        return `起送价￥ ${this.minPrice} 元`
+      } else if (this.totalPrice < this.minPrice) {
+        let diff = this.minPrice - this.totalPrice
+        return `还差￥ ${diff} 元`
+      } else {
+        return `去结算`
+      }
+    },
+    payClass () { // 控制样式，支付栏的钱够了背景绿
+      if (this.totalPrice < this.minPrice) {
+        return 'not-enough'
+      } else {
+        return 'enough'
+      }
     }
   }
 }
@@ -63,6 +106,21 @@ export default {
           width:56px
           height:56px
           box-sizing border-box
+          .num
+            position absolute
+            top:0
+            left:0
+            width:24px
+            height:16px
+            line-height 16px
+            font-size 9px
+            box-shadow 0 4px 8px 0 rgba(0,0,0,.4)
+            font-weight:700
+            background red
+            color: #ffffff
+            margin-left 28px
+            border-radius 6px
+            text-align center
           .logo
             width: 100%
             height: 100%
@@ -73,6 +131,11 @@ export default {
               font-size 24px
               color #80858a
               line-height 44px // 56 - 12
+              &.lightHeight
+                color: #ffffff
+            &.lightHeight
+              color: #ffffff
+              background rgb(0,160,200)
         .price
           display inline-block
           font-size 16px
@@ -84,6 +147,8 @@ export default {
           padding-right 12px
           border-right 1px solid rgba(255,255,255,.1)
           color: rgba(255,255,255,.4)
+          &.lightHeight
+            color: #ffffff
         .desc
           display inline-block
           font-size 16px
@@ -95,10 +160,15 @@ export default {
       .content-right
         flex 0 0 105px
         width:105px
-        text-align center
-        font-size 16px
-        color: rgba(255,255,255,.4)
-        background #2b343c
-        line-height 48px
-        vertical-align: top
+        .pay
+          text-align center
+          font-size 16px
+          color: rgba(255,255,255,.4)
+          line-height 48px
+          vertical-align: top
+          &.not-enough
+            background #2b343c
+          &.enough
+            background green
+            color: #ffffff
 </style>
